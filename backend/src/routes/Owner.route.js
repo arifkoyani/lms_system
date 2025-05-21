109a201da5422dd867c489781b6f42fd8b8708bb
arifkoyani <arifkoyani@gmail.com>
2025-05-21 18:33:36 +0200

    newcode

--- backend/src/routes/Owner.route.js
+++ backend/src/routes/Owner.route.js
@@ -7,7 +7,7 @@ import {
 import upload from "../middlewares/multer.middleware.js";
 const OwnerRoute = Router();
 
-OwnerRoute.route("/login").post(login);
+OwnerRoute.route("/login").post(login); //thi is user login data
 OwnerRoute.route("/register").post(upload.single("profile"), registerOwner);
 OwnerRoute.route("/upload").post(upload.single("profile"), imageUpload);
 export default OwnerRoute;
