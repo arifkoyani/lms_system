a34d27e3ce741dd4442173ddcc994feea44a25ef
arifkoyani <arifkoyani@gmail.com>
2025-05-22 18:03:01 +0200

    newcode

---
+++ frontEnd/src/components/login/login.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:4040/api/v1/owner/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (res.ok) {
+      alert("Login successful");
+    } else {
+      alert("Something went wrong");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleLogin}
+      className="max-w-sm mx-auto mt-10 p-6 bg-white  rounded-xl shadow-md space-y-4"
+    >
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="w-full px-4 text-black py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="w-full px-4 text-black py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-200"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
