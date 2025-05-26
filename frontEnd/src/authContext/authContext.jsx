e346c270ed1a82b87753687e90bc9660375dafac
arifkoyani <arifkoyani@gmail.com>
2025-05-26 18:36:23 +0200

    newcode

---
+++ frontEnd/src/authContext/authContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+// provider
+
+function AuthProvider({ children }) {
+  const persistValue = JSON.parse(localStorage.getItem("persist")) || true;
+
+  const [auth, setAuth] = useState({});
+  const [persist, setPersist] = useState(persistValue);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProvider };
