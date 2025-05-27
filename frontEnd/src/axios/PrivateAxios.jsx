55d88fa47a5923c640c341fa3abc91d2a7bbc9fb
arifkoyani <arifkoyani@gmail.com>
2025-05-27 17:48:09 +0200

    newcode

---
+++ frontEnd/src/axios/PrivateAxios.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../authContext/AuthContext";
+function PrivateAxios() {
+  const { auth } = useContext(AuthContext);
+  return axios.create({
+    baseURL: "http://localhost:7070/api/v1/owner",
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${auth.accessToken}`,
+    },
+  });
+}
+
+export default PrivateAxios;
