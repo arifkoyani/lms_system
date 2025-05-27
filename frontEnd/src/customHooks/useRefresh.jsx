2e1b6f1c96f8e6f15c96be8a5a91eb4a491bb58d
arifkoyani <arifkoyani@gmail.com>
2025-05-27 17:46:50 +0200

    newoce

---
+++ frontEnd/src/customHooks/useRefresh.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { AuthContext } from "../authContext/AuthContext";
+import axios from "axios";
+function useRefresh() {
+  const { setAuth, persist } = useContext(AuthContext);
+  return async function refresh() {
+    try {
+      const res = await axios.get(
+        "http://localhost:7070/api/v1/owner/refresh",
+        { withCredentials: true }
+      );
+
+      if (res?.data?.status === 1) {
+        if (!persist) {
+          console.log("persist", persist);
+          return;
+        }
+        setAuth((prev) => {
+          return {
+            ...prev,
+            accessToken: res?.data?.accessToken,
+          };
+        });
+
+        return res?.data?.accessToken;
+      }
+    } catch (error) {
+      console.error("Error refreshing token:", error);
+      setAuth((prev) => {
+        return {
+          ...prev,
+          accessToken: null,
+        };
+      });
+      return null;
+    }
+  };
+}
+
+export default useRefresh;
